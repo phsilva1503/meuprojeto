deac32f0fb425706e0e2dbfaf59b0a3367ecd9db
phsilva1503 <phsilva1@outlook.com.br>
2025-09-22 00:42:52 -0300

    AJUSTADO HTTP

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     with app.app_context():
         # db.drop_all()
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
